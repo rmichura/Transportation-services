cb4933dd3a0459f80628bde10e95c10196561501
rmichura <r.michura@wp.pl>
2022-04-03 14:21:21 +0200

    Added database src in class config.js

--- backend/app/config.js
+++ backend/app/config.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 module.exports = {
     port: process.env.PORT || 3000,
-    database: process.env.DATABASE || ''
+    database: process.env.DATABASE || 'mongodb+srv://user:user@cluster0.mhqbq.mongodb.net/transportation-services?retryWrites=true&w=majority'
 }
