779d3327cb45e42ea18e5b3d10ecadee95f27738
rmichura <r.michura@wp.pl>
2022-04-03 14:43:02 +0200

    Added password hashing and create apiToken in class models/user.js

--- backend/app/db/models/user.js
+++ backend/app/db/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const randomstring = require('randomstring');
 
 const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
@@ -25,7 +27,39 @@ const userSchema = new mongoose.Schema({
         trim: true,
         minlength: [4],
     },
+    apiToken: String,
 });
+
+userSchema.pre('save', function (next) {
+    const user = this
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(user.password, salt);
+    user.password = hash;
+    next();
+});
+
+userSchema.post('save', function (error, doc, next) {
+    if (error.code === 11000) {
+        error.errors = { email: {
+                message: 'We already have such an e-mail'
+            }};
+    }
+    next(error);
+})
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (user.isNew) {
+        user.apiToken = randomstring.generate(50);
+    }
+    next();
+})
+
+userSchema.methods = {
+    comparePassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
 
 const User = mongoose.model('User', userSchema)
 
