974bf7676ff37af714650ee2df024e08d4fac6a0
rmichura <r.michura@wp.pl>
2022-05-21 22:24:43 +0200

    Created login and register pages

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      class="fill-height"
+  >
+    <v-container class="fill-height">
+      <v-row class="justify-center">
+        <v-sheet
+            elevation="8"
+            height="400"
+            width="600"
+        >
+          <v-col class="emailMargin">
+            <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+            ></v-text-field>
+            <v-text-field
+                v-model="password"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show ? 'text' : 'password'"
+                :rules="[rules.required, rules.min]"
+                name="input-10-1"
+                label="Password"
+                @click:append="show = !show"
+                @keyup.enter="loginUser"
+            ></v-text-field>
+            <v-row class="justify-center">
+              <span class="without-account">
+                Jeśli nie masz konta, zarejestruj się
+              <router-link to="/register">click me</router-link>
+                </span>
+            </v-row>
+            <v-row class="justify-center ma-16">
+              <v-btn
+                  class="white--text"
+                  color="teal"
+                  elevation="7"
+                  large
+                  @click="loginUser"
+                  :disabled="!valid"
+              >Zaloguj się
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-sheet>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  name: 'Login',
+
+  data() {
+    return {
+      show: false,
+      email: '',
+      password: '',
+      valid: true,
+      emailRules: [
+        v => !!v || 'E-mail jest wymagany',
+        v => /.+@.+\..+/.test(v) || 'E-mail musi być poprawny',
+      ],
+      rules: {
+        required: value => !!value || 'Wymagany.',
+        min: v => v.length >= 8 || 'Min 8 znaków'
+      }
+    }
+  },
+  methods: {
+    async loginUser() {
+      if (this.email !== '') {
+        this.$refs.form.validate()
+        await this.$store.dispatch('login', {
+          email: this.email,
+          password: this.password
+        })
+        if (localStorage.getItem('token')) {
+          this.goTo()
+        } else {
+          alert('Nie mamy takiego konta')
+        }
+      } else {
+        alert('Coś poszło nie tak')
+      }
+    },
+    goTo() {
+      router.push('/customer')
+    }
+  },
+}
+</script>
+
+<style>
+
+.emailMargin {
+  margin-top: 2em;
+}
+
+.without-account {
+  text-align: center;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+  margin-top: 1em;
+}
+
+</style>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      class="fill-height"
+  >
+    <v-container class="fill-height">
+      <v-row class="justify-center">
+        <v-sheet
+            elevation="8"
+            height="400"
+            width="600"
+        >
+          <v-col class="emailMargin">
+            <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+            ></v-text-field>
+            <v-text-field
+                v-model="password"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show ? 'text' : 'password'"
+                :rules="[rules.required, rules.min]"
+                name="input-10-1"
+                label="Password"
+                @click:append="show = !show"
+            ></v-text-field>
+            <v-text-field
+                v-model="confirmPassword"
+                :append-icon="showConfirm ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="showConfirm ? 'text' : 'password'"
+                :rules="[rules.required, rules.min]"
+                name="input-10-1"
+                label="Confirm Password"
+                @click:append="showConfirm = !showConfirm"
+                @keyup.enter="creatUser"
+            ></v-text-field>
+            <v-row class="justify-center ma-10">
+              <v-btn
+                  class="white--text"
+                  color="teal"
+                  elevation="7"
+                  large
+                  @click="creatUser"
+                  :disabled="!valid"
+              >Zarejestruj się
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-sheet>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import router from "@/router"
+
+export default {
+  name: 'Register',
+
+  data() {
+    return {
+      show: false,
+      showConfirm: false,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: true,
+      emailRules: [
+        v => !!v || 'E-mail jest wymagany',
+        v => /.+@.+\..+/.test(v) || 'E-mail musi być poprawny',
+      ],
+      rules: {
+        required: value => !!value || 'Wymagany.',
+        min: v => v.length >= 8 || 'Min 8 znaków'
+      }
+    }
+  },
+  methods: {
+    async creatUser() {
+      if (this.email !== '' && this.password === this.confirmPassword) {
+        this.$refs.form.validate()
+        await this.$store.dispatch('register', {
+          email: this.email,
+          password: this.password
+        })
+        this.goToLogin()
+      } else {
+        alert('Coś poszło nie tak')
+      }
+    },
+    goToLogin() {
+      router.push('/login')
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
