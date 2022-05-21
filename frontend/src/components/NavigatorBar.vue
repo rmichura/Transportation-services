75ed53ae8e135e9dee8302cc9cccbf4724690c9a
rmichura <r.michura@wp.pl>
2022-05-21 22:23:48 +0200

    Created footer and navigator-bar in components

---
+++ frontend/src/components/NavigatorBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app-bar
+      app
+      color="teal"
+      dark
+  >
+    <router-link to="/" class="teal--text">
+      <v-btn
+          text
+          v-if="!auth"
+      >
+        <span class="mr-2">Transportation services</span>
+      </v-btn>
+    </router-link>
+
+    <router-link to="/customer" class="teal--text">
+      <v-btn
+          text
+          v-if="auth"
+      >
+        <span class="mr-2">Transportation services</span>
+      </v-btn>
+    </router-link>
+
+    <v-spacer></v-spacer>
+
+    <router-link to="/login" class="teal--text">
+      <v-btn
+          text
+          v-if="!auth"
+      >
+        <span class="mr-2">Zaloguj się</span>
+        <v-icon>mdi-login</v-icon>
+      </v-btn>
+    </router-link>
+
+    <router-link to="/" class="teal--text">
+      <v-btn
+          text
+          v-if="auth"
+          @click="logOut"
+      >
+        <span class="mr-2">Wyloguj się</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </router-link>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavigatorBar",
+
+  computed: {
+    auth() {
+      return this.$store.getters.isAuth;
+    }
+  },
+
+  methods: {
+    logOut() {
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer
+      dark
+      padless
+  >
+    <v-card
+        class="flex"
+        flat
+        tile
+    >
+      <v-card-text class="text-center white--text teal">
+        <strong>2022 - Transportation services</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
