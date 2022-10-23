ec5bf42a4ecd44adf1c978f7522e2a83b9383a39
rmichura <r.michura@wp.pl>
2022-10-23 21:34:33 +0200

    Corrected order model because all field is required now

--- backend/app/db/models/order.js
+++ backend/app/db/models/order.js
@@ -4,31 +4,40 @@ const orderSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     productType: {
         type: String,
+        required: true,
     },
     productWeight: {
         type: Number,
+        required: true,
     },
     productWidth: {
         type: Number,
+        required: true,
     },
     productHeight: {
         type: Number,
+        required: true,
     },
     receptionVenue: {
         type: String,
+        required: true,
     },
     destination: {
         type: String,
+        required: true,
     },
     status: {
       type: String,
+        required: true,
     },
     deadline: {
         type: Date,
+        required: true,
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+        required: true,
     },
 });
 const Order = mongoose.model('Order', orderSchema)
