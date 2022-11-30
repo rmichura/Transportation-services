cf75195dc90dba035f955e721161817411711f31
rmichura <r.michura@wp.pl>
2022-11-30 19:21:59 +0100

    Added dialog and view to orders management

---
+++ frontend/src/dialogs/OrderManagement.vue
@@ -0,0 +1,370 @@
+<template>
+  <v-container v-if="getCars">
+    <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        class="fill-height"
+    >
+      <v-dialog
+          v-model="dialog"
+          max-width="700"
+          persistent
+      >
+        <v-card>
+          <v-card-title>
+            <p class="text-dialog">Zamówienie: <span>{{ this.currentOrder.numberOrder }}</span></p>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+                icon
+                @click="this.closeDialog"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.productType"
+                      color="teal"
+                      :rules="required"
+                      readonly
+                      label="Rodzaj towaru"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.productWeight"
+                      color="teal"
+                      :rules="required"
+                      type="number"
+                      readonly
+                      label="Waga towaru - kg"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.receptionVenue"
+                      color="teal"
+                      :rules="required"
+                      readonly
+                      label="Miejsce odbioru"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.destination"
+                      color="teal"
+                      :rules="required"
+                      readonly
+                      label="Miejsce docelowe"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.productWidth"
+                      color="teal"
+                      :rules="required"
+                      type="number"
+                      readonly
+                      label="Szerokość towaru - cm"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="4"
+                    md="3"
+                >
+                  <v-text-field
+                      v-model="currentOrder.productHeight"
+                      color="teal"
+                      :rules="required"
+                      type="number"
+                      readonly
+                      label="Wysokość towaru - cm"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-menu
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                          v-model="currentOrder.deadline"
+                          label="Termin dostarczenia"
+                          prepend-icon="mdi-calendar"
+                          readonly
+                          color="teal"
+                      ></v-text-field>
+                    </template>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <div v-if="refreshCars"></div>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-select
+                      dense
+                      color="teal"
+                      :rules="required"
+                      :items="items"
+                      item-color="teal"
+                      class="margin-select"
+                      label="Wybierz samochód"
+                      @change="selectedCar($event)"
+                      v-model="select"
+                  ></v-select>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="currentCar.brand"
+                      color="teal"
+                      readonly
+                      label="Marka"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="currentCar.type"
+                      color="teal"
+                      label="Typ"
+                      readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="currentCar.capacity"
+                      color="teal"
+                      type="number"
+                      label="Ładowność - kg"
+                      readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="currentCar.maxWidth"
+                      color="teal"
+                      type="number"
+                      label="Dopuszczlna szerokość - cm"
+                      readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                >
+                  <v-text-field
+                      v-model="currentCar.maxHeight"
+                      color="teal"
+                      type="number"
+                      label="Dopuszczalna wysokość - cm"
+                      readonly
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <div class="buttons-dialog">
+              <v-btn
+                  text
+                  color="teal"
+                  @click="closeDialog"
+              >
+                Anuluj
+              </v-btn>
+              <v-btn
+                  class="white--text ma-2"
+                  color="teal"
+                  elevation="3"
+                  large
+                  :disabled="!valid"
+                  @click="editOrder"
+              >
+                Zapisz
+              </v-btn>
+            </div>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-form>
+    <v-snackbar
+        v-model="snackBarError"
+        right
+        height="100"
+        color="error"
+        rounded
+        elevation="8"
+    >
+      Błąd! Wypełnij wszystkie pola poprawnie!
+    </v-snackbar>
+    <v-snackbar
+        v-model="snackbarSuccess"
+        right
+        height="100"
+        color="green"
+        rounded
+        elevation="8"
+    >
+      Gratulacje! Poprawnie przydzieliłeś samochód!
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "OrderManagement",
+  props: ['dialog', 'currentOrder'],
+  data() {
+    return {
+      valid: true,
+      menu: false,
+      snackBarError: false,
+      snackbarSuccess: false,
+      dialogValue: this.dialog,
+      select: null,
+      cars: [],
+      currentCar: [],
+      items: [],
+      required: [
+        v => !!v || 'Pole jest wymagane',
+      ]
+    }
+  },
+  computed: {
+    getCars() {
+      return this.$store.dispatch('getCars')
+    },
+    refreshCars() {
+      this.cars = this.$store.getters.cars
+      this.items = [];
+      this.cars.forEach(data => {
+        if (data.status === 'wolny') {
+          this.items.push(data.numberCar)
+        }
+      })
+      return this.items
+    }
+  },
+  methods: {
+    editOrder() {
+      if (this.$refs.form.validate()) {
+        const formData = new FormData();
+        this.currentOrder.car = this.currentCar._id
+        this.currentOrder.status = 'in_road'
+        formData.append('status', 'zajęty')
+        this.$store.dispatch('updateOrder', [this.currentOrder._id, this.currentOrder])
+        this.$store.dispatch('updateCar', [this.currentCar._id, formData]).then(() => {
+          this.$store.dispatch('getCars')
+        })
+        this.closeDialog()
+        this.snackbarSuccess = true
+      } else {
+        this.snackBarError = true
+      }
+    },
+    closeDialog() {
+      this.dialogValue = false
+      this.$emit('update:dialog', this.dialogValue)
+      this.$refs.form.resetValidation()
+      this.currentCar = [];
+      this.select = null;
+    },
+    selectedCar(value) {
+      this.currentCar = [];
+      this.cars.forEach(data => {
+        if (data.numberCar === value && data.status === 'wolny') {
+          this.currentCar.push(data)
+          this.currentCar = this.currentCar[0]
+        }
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+.text-dialog {
+  font-size: 1.1em;
+  margin-left: 0.5em;
+  font-family: Arial, serif;
+}
+
+.text-dialog span {
+  color: teal;
+}
+
+.buttons-dialog {
+  margin-right: 1em;
+  margin-bottom: 1em;
+  margin-top: -1em;
+}
+
+.margin-select {
+  margin-top: 1.4em
+}
+</style>
--- frontend/src/views/employee/EmployeeOrdersManagement.vue
+++ frontend/src/views/employee/EmployeeOrdersManagement.vue
@@ -1,15 +1,149 @@
 <template>
-  <v-container>
-    hello ordersss
+  <v-container v-if="getOrders">
+    <v-row class="justify-center">
+      <p class="number-order">Obsługa zamówi<span>eń</span></p>
+    </v-row>
+
+    <div v-if="setOrderInTable"></div>
+
+    <v-text-field
+        v-model="search"
+        label="Szukaj zamówienia"
+        single-line
+        hide-details
+        color="teal"
+    ></v-text-field>
+
+    <v-data-table
+        :headers="headers"
+        :items="orders"
+        class="data-table elevation-1"
+        :page.sync="page"
+        :search="search"
+        :items-per-page="ordersPerPage"
+        disable-sort
+        hide-default-footer
+    >
+      <template v-slot:body="{ items }">
+        <tbody>
+        <tr :class="index === selectedRow ? 'custom-highlight-row' : ''"
+            v-for="(order, index) in orders" :key="order.name">
+          <td>{{ order.numberOrder }}</td>
+          <td>{{ order.productType }}</td>
+          <td>{{ order.receptionVenue }}</td>
+          <td>{{ order.destination }}</td>
+          <td v-if="order.status === 'in_progress'">
+            <v-btn
+                icon
+                @click="openDialog(index)"
+            >
+              <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </td>
+          <td v-else>
+            <v-btn
+                icon
+                @click="orderFinish(index)"
+            >
+              <v-icon>mdi-clipboard-check-outline</v-icon>
+            </v-btn>
+          </td>
+        </tr>
+        </tbody>
+      </template>
+    </v-data-table>
+
+    <v-pagination
+        v-model="page"
+        :length="pageCount"
+        class="ma-2"
+        color="teal"
+    ></v-pagination>
+
+    <order-management :dialog.sync="dialog" :current-order="currentOrder"/>
+
   </v-container>
 </template>
 
 <script>
+import OrderManagement from "@/dialogs/OrderManagement";
+
 export default {
-  name: "EmployeeOrdersManagement"
+  name: "EmployeeOrdersManagement",
+  components: {OrderManagement},
+  data() {
+    return {
+      selectedRow: null,
+      orders: [],
+      dialog: false,
+      page: 1,
+      ordersPerPage: 10,
+      search: '',
+      currentOrder: [],
+      headers: [
+        {text: 'Numer zamówienia', value: 'numberOrder'},
+        {text: 'Rodzaj towaru', value: 'productType'},
+        {text: 'Miejsce odbioru', value: 'receptionVenue'},
+        {text: 'Miejsce docelowe', value: 'destination'},
+        {text: 'Więcej'}
+      ],
+    }
+  },
+  computed: {
+    getOrders() {
+      return this.$store.dispatch('getAllOrders')
+    },
+    setOrderInTable() {
+      let allOrder = this.$store.getters.allOrders
+      this.orders = []
+      allOrder.forEach(data => {
+        if (data.status === 'in_progress' || data.status === 'in_road' && data.deadline) {
+          data.deadline = data.deadline.toString().split('T')[0]
+          this.orders.push(data)
+        }
+      })
+      return this.orders
+    },
+    pageCount() {
+      return Math.ceil(this.orders.length / this.ordersPerPage)
+    },
+  },
+  methods: {
+    openDialog(index) {
+      this.currentOrder = this.orders[index]
+      this.dialog = true;
+    },
+    orderFinish(index) {
+      const id = this.orders[index]._id
+      this.orders[index].status = 'finished'
+      this.$store.dispatch('updateOrder', [id, this.orders[index]])
+    }
+  }
 }
 </script>
 
 <style scoped>
+.number-order {
+  font-size: 1.7em;
+  font-family: Arial, serif;
+  margin-top: 8vh;
+  margin-bottom: 8vh;
+}
 
+.number-order span {
+  color: teal;
+}
+
+.data-table {
+  font-size: 2em;
+  margin-top: 5vh;
+}
+
+.custom-highlight-row {
+  background-color: #eeeeee;
+}
+
+.v-data-table::v-deep th {
+  font-size: 0.5em !important;
+}
 </style>
